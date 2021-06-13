5b5b75213c70835334de4a02600517443386fe3d
keerthana-2001 <73514602+keerthana-2001@users.noreply.github.com>
2021-06-13 22:53:04 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,12 +1,6 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
-# include<cs50.h>
 # include<assert.h>
-int max(int [],int);
-int min(int [],int);
-float average(int [],int);
-int mode(int [],int);
-int factors(int,int []);
 int max(int a[],int n){
 int m=a[0];
 for(int i=0;i<n;i++){
@@ -69,35 +63,4 @@ num=num/i;
 }
 }
 return count;
-}
-int main(void){
-int x[]={9,5,6,10,2,-3,4};
-assert(max(x,7)==10);
-int y[]={5};
-assert(max(y,1)==5);
-printf("Max: passed\n");
-assert(min(x,7)==-3);
-assert(min(y,1)==5);
-printf("Min: passed\n");
-assert((average(x,7)-4.7142)<0.001);
-assert(average(y,1)==5.0);
-printf("Average: passed\n");
-int v[]={4, 9,5,6,5,10,0,2,-3, -3,4, 4};
-assert((mode(v, 12) == 4));
-int z[]={5};
-assert(mode(z, 1) == 5);
-printf("Mode: passed\n");
-int ret[100] = {0};
-int count = factors(180, ret);
-assert (count == 5);
-assert (ret[0] == 2);
-assert (ret[1] == 2);
-assert (ret[2] == 3);
-assert (ret[3] == 3);
-assert (ret[4] == 5);
-count = factors(143, ret);
-assert (count == 2);
-assert (ret[0] == 11);
-assert (ret[1] == 13);
-printf("Factors: passed\n");
 }
